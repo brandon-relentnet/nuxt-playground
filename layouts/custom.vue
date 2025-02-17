48b68a94b0402d4ce529bc60e5fff605e01a0546
Brandon Harris <brandon@relentnet.com>
2025-02-17 15:51:07 -0600

    You already know I had to install Tailwind, the greatest CSS tool ever.

--- layouts/custom.vue
+++ layouts/custom.vue
@@ -1,14 +1,14 @@
 <template>
-    <div>
-        <header>
-        <h1>My Custom Layout</h1>
-        <nav>
-            <NuxtLink to="/">Home</NuxtLink> |
-            <NuxtLink to="/about">About</NuxtLink>
-        </nav>
-        </header>
-        <main>
-        <slot />
-        </main>
-    </div>
+  <div>
+    <header>
+      <h1 class="text-red-500">My Custom Layout</h1>
+      <nav>
+        <NuxtLink to="/">Home</NuxtLink> |
+        <NuxtLink to="/about">About</NuxtLink>
+      </nav>
+    </header>
+    <main>
+      <slot />
+    </main>
+  </div>
 </template>
