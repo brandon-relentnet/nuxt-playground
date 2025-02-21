1d47d46c2ff90a924d3c4e907d5f6cf8ec2af7d4
Brandon Harris <brandon@relentnet.com>
2025-02-21 14:18:29 -0600

    Styled the dropdown by introducing sections and icons. Gonna continue to see how I can slim this code.

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,29 +1,76 @@
 <script setup>
 import { ref, onMounted } from "vue";
 import autoAnimate from "@formkit/auto-animate";
-import { PlayIcon } from "@heroicons/vue/24/solid";
+import {
+  PlayIcon,
+  MagnifyingGlassIcon,
+  ArrowPathIcon,
+  GlobeAltIcon,
+  PencilIcon,
+  WrenchIcon,
+  DocumentIcon,
+  ChartBarIcon,
+  MegaphoneIcon,
+  ShoppingCartIcon,
+  ServerIcon,
+  CloudIcon,
+  CubeIcon,
+  LockClosedIcon,
+  ShieldCheckIcon,
+} from "@heroicons/vue/24/solid";
 
 const navItems = ref([
   {
     label: "Domains",
     dropdown: true,
     open: false,
-    items: [
-      { label: "Register", url: "https://control.relentnet/register" },
-      { label: "Transfer", url: "https://control.relentnet/transfer" },
+    sections: [
+      {
+        sectionLabel: "Find a Domain",
+        links: [
+          {
+            label: "Search for Domain Names",
+            url: "https://control.relentnet.com/cart.php?a=add&domain=register",
+            icon: MagnifyingGlassIcon,
+          },
+          {
+            label: "Transfer Domain Names",
+            url: "https://control.relentnet.com/cart.php?a=add&domain=transfer",
+            icon: ArrowPathIcon,
+          },
+          {
+            label: "gTLD Domain Extensions",
+            url: "https://control.relentnet.com/cart.php?a=add&domain=register",
+            icon: GlobeAltIcon,
+          },
+        ],
+      },
     ],
   },
   {
     label: "Websites",
     dropdown: true,
     open: false,
-    items: [
-      { label: "Website Builder", url: "#" },
-      { label: "Website Templates", url: "#" },
-      { label: "Online Store", url: "#" },
-      { label: "Website Design Services", url: "#" },
-      { label: "SEO Services", url: "#" },
-      { label: "Digital Marketing Services", url: "#" },
+    sections: [
+      {
+        sectionLabel: "Build & Design",
+        links: [
+          { label: "Website Design Services", url: "#", icon: PencilIcon },
+          { label: "Website Builder", url: "#", icon: WrenchIcon },
+          { label: "Website Templates", url: "#", icon: DocumentIcon },
+        ],
+      },
+      {
+        sectionLabel: "Services",
+        links: [
+          { label: "SEO Services", url: "#", icon: ChartBarIcon },
+          { label: "Digital Marketing Services", url: "#", icon: MegaphoneIcon },
+        ],
+      },
+      {
+        sectionLabel: "E-commerce",
+        links: [{ label: "Online Store", url: "#", icon: ShoppingCartIcon }],
+      },
     ],
   },
   {
@@ -35,12 +82,26 @@ const navItems = ref([
     label: "Hosting and Security",
     dropdown: true,
     open: false,
-    items: [
-      { label: "Wordpress Hosting", url: "#" },
-      { label: "Web Hosting", url: "#" },
-      { label: "VPS Hosting", url: "#" },
-      { label: "SSL Certificates", url: "#" },
-      { label: "Website Security", url: "#" },
+    sections: [
+      {
+        sectionLabel: "Hosting",
+        links: [
+          { label: "WordPress Hosting", url: "#", icon: ServerIcon },
+          { label: "Web Hosting", url: "#", icon: CloudIcon },
+          { label: "VPS Hosting", url: "#", icon: CubeIcon },
+        ],
+      },
+      {
+        sectionLabel: "Security",
+        links: [
+          {
+            label: "SSL Certificates",
+            url: "https://control.relentnet.com/index.php?rp=/store/ssl-certificates",
+            icon: LockClosedIcon,
+          },
+          { label: "Website Security", url: "#", icon: ShieldCheckIcon },
+        ],
+      },
     ],
   },
   {
@@ -62,12 +123,12 @@ function setDropdownRef(el, index) {
 }
 
 onMounted(() => {
-  dropdownRefs.value.forEach(el => {
+  dropdownRefs.value.forEach((el) => {
     if (el) autoAnimate(el);
   });
 });
 
-function toggleDropdown(item, index) {
+function toggleDropdown(item) {
   item.open = !item.open;
 }
 
@@ -84,7 +145,7 @@ function closeDropdown(item) {
           <template v-if="item.dropdown">
             <div v-on-click-outside.bubble="() => closeDropdown(item)">
               <button
-                @click="toggleDropdown(item, index)"
+                @click="toggleDropdown(item)"
                 class="focus:outline-none hover:bg-surface rounded-xl px-4 py-2 transition-colors duration-200 cursor-pointer"
                 :class="{ 'bg-surface': item.open }"
               >
@@ -95,22 +156,39 @@ function closeDropdown(item) {
                 />
               </button>
               <div
-                :ref="el => setDropdownRef(el, index)"
+                :ref="(el) => setDropdownRef(el, index)"
                 :id="'dropdown-' + item.label.replace(/\s+/g, '-')"
-                class="fixed flex top-20 left-0 w-full bg-surface shadow-lg dropdown-menu overflow-hidden transition-all duration-300"
-                :style="item.open ? { height: '250px' } : { height: '0', padding: '0', border: 'none' }"
+                class="fixed top-20 left-0 w-full bg-surface shadow-lg dropdown-menu overflow-hidden transition-all duration-300"
+                :class="item.open ? 'h-[275px] p-4' : 'h-0 p-0'"
               >
-                <ul v-if="item.open">
-                  <li
-                    v-for="(child, idx) in item.items"
-                    :key="idx"
-                    class="py-1"
+                <div
+                  v-if="item.open"
+                  class="w-full flex flex-wrap justify-start gap-x-12 px-8"
+                >
+                  <div
+                    v-for="(section, sIndex) in item.sections"
+                    :key="sIndex"
+                    class="min-w-[200px] p-4"
                   >
-                    <a :href="child.url" class="block hover:text-blue-600">
-                      {{ child.label }}
-                    </a>
-                  </li>
-                </ul>
+                    <SectionTitle>
+                      {{ section.sectionLabel }}
+                    </SectionTitle>
+                    <ul class="gap-y-2 flex flex-col">
+                      <NuxtLink
+                        v-for="(link, linkIndex) in section.links"
+                        :key="linkIndex"
+                        class="py-2 group hover:bg-overlay rounded-xl px-4 -ml-4"
+                        :to="link.url"
+                      >
+                        <component
+                          :is="link.icon"
+                          class="size-6 text-rose inline-block mr-2 -mt-0.5"
+                        />
+                        <span class="group-hover:text-foam">{{ link.label }}</span>
+                      </NuxtLink>
+                    </ul>
+                  </div>
+                </div>
               </div>
             </div>
           </template>
