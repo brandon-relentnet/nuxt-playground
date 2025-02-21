7d3908d833ba82eda6b578fd9cbb254a49b880ba
Brandon Harris <brandon@relentnet.com>
2025-02-20 22:18:43 -0600

    Added the links I want in the navbar and styled it to my liking.

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,21 +1,117 @@
 <script setup>
-import { ref } from 'vue';
+import { ref } from "vue";
+import { PlayIcon } from "@heroicons/vue/24/solid";
 
-const dropdown = ref(false);
+const navItems = ref([
+  {
+    label: "Domains",
+    dropdown: true,
+    open: false,
+    items: [
+      { label: "Register", url: "https://control.relentnet/register" },
+      { label: "Transfer", url: "https://control.relentnet/transfer" },
+    ],
+  },
+  {
+    label: "Websites",
+    dropdown: true,
+    open: false,
+    items: [
+      { label: "Website Builder", url: "#" },
+      { label: "Website Templates", url: "#" },
+      { label: "Online Store", url: "#" },
+      { label: "Website Design Services", url: "#" },
+      { label: "SEO Services", url: "#" },
+      { label: "Digital Marketing Services", url: "#" },
+    ],
+  },
+  {
+    label: "Email",
+    dropdown: false,
+    url: "#",
+  },
+  {
+    label: "Hosting and Security",
+    dropdown: true,
+    open: false,
+    items: [
+      { label: "Wordpress Hosting", url: "#" },
+      { label: "Web Hosting", url: "#" },
+      { label: "VPS Hosting", url: "#" },
+      { label: "SSL Certificates", url: "#" },
+      { label: "Website Security", url: "#" },
+    ],
+  },
+  {
+    label: "About",
+    dropdown: false,
+    url: "#",
+  },
+  {
+    label: "Contact",
+    dropdown: false,
+    url: "#",
+  },
+]);
+
+function toggleDropdown(item) {
+  item.open = !item.open;
+}
+
+function closeDropdown(item) {
+  item.open = false;
+}
 </script>
 
 <template>
-    <div class="bg-base text-text h-20 flex justify-between items-center px-8 fixed top-0 left-0 w-full z-50">
-        <div class="ml-2 relative inline-block">
-            <button @click="dropdown = !dropdown">
-                Toggle Dropdown
+  <nav class="bg-base text-lg text-text fixed top-0 left-0 w-full z-50 px-8">
+    <ul class="flex space-x-4 items-center h-20">
+      <li v-for="(item, index) in navItems" :key="index" class="relative">
+        <template v-if="item.dropdown">
+          <div v-on-click-outside.bubble="() => closeDropdown(item)">
+            <button
+              @click="toggleDropdown(item)"
+              class="focus:outline-none hover:bg-surface rounded-xl px-4 py-2 transition-colors duration-200 cursor-pointer"
+              :class="{ 'bg-surface': item.open }"
+            >
+              {{ item.label }}
+              <PlayIcon
+              class="size-3 inline-block -rotate-90 transition duration-200"
+              :class="{
+                'rotate-90': item.open,
+              }"
+            />
             </button>
-            <div 
-            v-if="dropdown"
-            v-on-click-outside="() => { dropdown = false }"
-            class="absolute bg-surface w-48 h-48 dropdown">
-                Dropdown Content
+            <div
+              v-if="item.open"
+              class="fixed top-20 left-0 w-full bg-surface shadow-lg p-4"
+            >
+              <ul>
+                <li v-for="(child, idx) in item.items" :key="idx" class="py-1">
+                  <a :href="child.url" class="block hover:text-blue-600">
+                    {{ child.label }}
+                  </a>
+                </li>
+              </ul>
             </div>
-        </div>
-    </div>
+          </div>
+        </template>
+
+        <template v-else>
+          <NuxtLink
+            :to="item.url"
+            class="focus:outline-none hover:bg-surface rounded-xl px-4 py-2 transition-colors duration-200 cursor-pointer"
+          >
+            {{ item.label }}
+          </NuxtLink>
+        </template>
+      </li>
+    </ul>
+  </nav>
 </template>
+
+<style scoped>
+nav {
+  font-family: "AdamBold", sans-serif;
+}
+</style>
